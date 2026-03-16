0bed84af6f5568d6b1fab562c76002185f9bc8fb
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-16 11:31:39 +0700

    pu

--- src/main.cob
+++ src/main.cob
@@ -1,9 +1,6 @@
        class-id. Program.
        method-id. Main static.
-           *> Gọi InsertEmployee
            invoke type EmployeeRepo "InsertEmployee" using "Dang", 30.
-
-           *> Gọi FetchEmployees
            invoke type EmployeeRepo "FetchEmployees".
            goback.
        end method Main.

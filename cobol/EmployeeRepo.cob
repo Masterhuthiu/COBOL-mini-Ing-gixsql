f82a9b4c4bfd336975c00a113b91264fa5498168
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-16 18:38:52 +0700

    pu

--- cobol/EmployeeRepo.cob
+++ cobol/EmployeeRepo.cob
@@ -1,16 +1,32 @@
-      ******************************************************************
-      * Author:
-      * Date:
-      * Purpose:
-      * Tectonics: cobc
-      ******************************************************************
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. YOUR-PROGRAM-NAME.
-       DATA DIVISION.
-       FILE SECTION.
-       WORKING-STORAGE SECTION.
-       PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-            DISPLAY "Hello world"
-            STOP RUN.
-       END PROGRAM YOUR-PROGRAM-NAME.
+       class-id. ExampleClass inherits Base.
+
+       factory.
+       data division.
+       working-storage section.
+       01 StaticMessage string value "Xin chào từ Factory!".
+
+       procedure division.
+       method-id. NewExample static.
+           procedure division returning obj as ExampleClass.
+               set obj to new ExampleClass
+           end method NewExample.
+
+       method-id. PrintStaticMessage static.
+           procedure division.
+               display StaticMessage
+           end method PrintStaticMessage.
+       end factory.
+
+       object. implements IInterfaceName.
+       data division.
+       working-storage section.
+       01 InstanceMessage string value "Xin chào từ Object!".
+
+       procedure division.
+       method-id. InstanceMethod.
+           procedure division.
+               display InstanceMessage
+           end method InstanceMethod.
+       end object.
+
+       end class ExampleClass.

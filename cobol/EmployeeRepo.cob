f869dcaf6656481f63b56afc7e07081afb88f1d4
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-16 17:51:30 +0700

    pu

--- cobol/EmployeeRepo.cob
+++ cobol/EmployeeRepo.cob
@@ -5,31 +5,25 @@
        CONFIGURATION SECTION.
 
        STATIC.
-           *> Phương thức Add: In thông báo đã add
            METHOD-ID. InsertEmployee.
            DATA DIVISION.
            LINKAGE SECTION.
                01 empName AS ANY.
                01 empAge BINARY-LONG.
            PROCEDURE DIVISION USING BY VALUE empName, empAge.
-               
+
                DISPLAY "------------------------------------".
-               DISPLAY "COBOL ENGINE: Bat dau xu ly..." .
-               DISPLAY "Hanh dong: DA ADD nhan vien " empName.
+               DISPLAY "COBOL: DA ADD nhan vien " empName.
                DISPLAY "Tuoi: " empAge.
-               DISPLAY "Trang thai: Thanh cong".
                DISPLAY "------------------------------------".
 
            END METHOD InsertEmployee.
 
-           *> Phương thức List: In thông báo đã list
            METHOD-ID. FetchEmployees.
            PROCEDURE DIVISION.
-               
+
                DISPLAY "------------------------------------".
-               DISPLAY "COBOL ENGINE: Truy xuat du lieu...".
-               DISPLAY "Hanh dong: DA LIST danh sach nhan vien".
-               DISPLAY "He thong: Dang cho phan hoi tu Database".
+               DISPLAY "COBOL: DA LIST danh sach nhan vien".
                DISPLAY "------------------------------------".
 
            END METHOD FetchEmployees.

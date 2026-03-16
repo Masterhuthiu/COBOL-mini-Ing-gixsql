edc10ff560d9ed761fd9b7de91accb3a6f4d9f88
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-16 18:46:04 +0700

    pu

--- cobol/EmployeeRepo.cob
+++ cobol/EmployeeRepo.cob
@@ -1,32 +1,36 @@
-       class-id. ExampleClass inherits Base.
+       class-id. EmployeeRepo.
 
        factory.
        data division.
        working-storage section.
-       01 StaticMessage string value "Xin chào từ Factory!".
+       01 StaticMessage pic x(50) value "Xin chao tu Factory.".
 
        procedure division.
-       method-id. NewExample static.
-           procedure division returning obj as ExampleClass.
-               set obj to new ExampleClass
-           end method NewExample.
-
        method-id. PrintStaticMessage static.
            procedure division.
                display StaticMessage
            end method PrintStaticMessage.
        end factory.
 
-       object. implements IInterfaceName.
+       object.
        data division.
        working-storage section.
-       01 InstanceMessage string value "Xin chào từ Object!".
+       01 Name pic x(50).
+       01 Age pic 9(4).
 
        procedure division.
-       method-id. InstanceMethod.
+       method-id. SetEmployee.
+           procedure division using by value newName as string
+                                      by value newAge as binary-long.
+               move newName to Name
+               move newAge to Age
+           end method SetEmployee.
+
+       method-id. PrintEmployee.
            procedure division.
-               display InstanceMessage
-           end method InstanceMethod.
+               display "Ten: " & Name
+               display "Tuoi: " & Age
+           end method PrintEmployee.
        end object.
 
-       end class ExampleClass.
+       end class EmployeeRepo.

4f0d00dda10adb2e7f2dc71d14a139c880527176
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-16 15:42:40 +0700

    pu

---
+++ cobol/main.cbl
@@ -0,0 +1,7 @@
+class-id. Program.
+method-id. Main static.
+    invoke type EmployeeRepo "InsertEmployee" using "Dang", 30.
+    invoke type EmployeeRepo "FetchEmployees".
+    goback.
+end method Main.
+end class Program.

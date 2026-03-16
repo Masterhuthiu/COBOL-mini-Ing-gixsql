3dceda895a2fb7a557e4b2e579cbec56909834e7
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-16 18:02:25 +0700

    pu

--- cobol/EmployeeRepo.cob
+++ cobol/EmployeeRepo.cob
@@ -1,32 +1,32 @@
-       IDENTIFICATION DIVISION.
-       CLASS-ID. EmployeeRepo.
+IDENTIFICATION DIVISION.
+CLASS-ID. EmployeeRepo.
 
-       ENVIRONMENT DIVISION.
-       CONFIGURATION SECTION.
+ENVIRONMENT DIVISION.
+CONFIGURATION SECTION.
 
-       STATIC.
-           METHOD-ID. InsertEmployee.
-           DATA DIVISION.
-           LINKAGE SECTION.
-               01 empName AS ANY.
-               01 empAge BINARY-LONG.
-           PROCEDURE DIVISION USING BY VALUE empName, empAge.
+STATIC.
+    METHOD-ID. InsertEmployee.
+    DATA DIVISION.
+    LINKAGE SECTION.
+        01 empName AS ANY.
+        01 empAge BINARY-LONG.
+    PROCEDURE DIVISION USING BY VALUE empName, empAge.
 
-               DISPLAY "------------------------------------".
-               DISPLAY "COBOL: DA ADD nhan vien " empName.
-               DISPLAY "Tuoi: " empAge.
-               DISPLAY "------------------------------------".
+        DISPLAY "------------------------------------".
+        DISPLAY "COBOL: DA ADD nhan vien " empName.
+        DISPLAY "Tuoi: " empAge.
+        DISPLAY "------------------------------------".
 
-           END METHOD InsertEmployee.
+    END METHOD InsertEmployee.
 
-           METHOD-ID. FetchEmployees.
-           PROCEDURE DIVISION.
+    METHOD-ID. FetchEmployees.
+    PROCEDURE DIVISION.
 
-               DISPLAY "------------------------------------".
-               DISPLAY "COBOL: DA LIST danh sach nhan vien".
-               DISPLAY "------------------------------------".
+        DISPLAY "------------------------------------".
+        DISPLAY "COBOL: DA LIST danh sach nhan vien".
+        DISPLAY "------------------------------------".
 
-           END METHOD FetchEmployees.
-       END STATIC.
+    END METHOD FetchEmployees.
+END STATIC.
 
-       END CLASS EmployeeRepo.
+END CLASS EmployeeRepo.

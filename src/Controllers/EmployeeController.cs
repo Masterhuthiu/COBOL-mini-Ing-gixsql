6b6aefc25dd2b9f49b68b9f4cd09882edd58a307
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-16 15:48:13 +0700

    pu

--- src/Controllers/EmployeeController.cs
+++ src/Controllers/EmployeeController.cs
@@ -14,7 +14,7 @@ namespace MiniIngenium.Controllers
         public IActionResult AddEmployee([FromBody] EmployeeRequest req)
         {
             // Gọi trực tiếp phương thức static trong EmployeeRepo.cbl
-            EmployeeRepo.InsertEmployee(req.Name, req.Age);
+            // EmployeeRepo.InsertEmployee(req.Name, req.Age);
             return Ok(new { message = "Thêm nhân viên thành công từ COBOL logic!" });
         }
     }

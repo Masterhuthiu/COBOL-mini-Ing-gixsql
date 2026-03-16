c5c0053ee8b908438637ddc49084e1f50d8b540f
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-16 15:46:11 +0700

    pu

---
+++ src/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using MiniIngenium.Services;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace MiniIngenium.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpPost("add")]
+        public IActionResult AddEmployee([FromBody] EmployeeRequest req)
+        {
+            // Gọi trực tiếp phương thức static trong EmployeeRepo.cbl
+            EmployeeRepo.InsertEmployee(req.Name, req.Age);
+            return Ok(new { message = "Thêm nhân viên thành công từ COBOL logic!" });
+        }
+    }
+}
